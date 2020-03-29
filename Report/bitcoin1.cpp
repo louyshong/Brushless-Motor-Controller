bafe262bd9532bd36597bd062cde9e528812e467
Louys Hong <louyshong1998@gmail.com>
2020-03-29 21:57:46 +0100

    edit code snippet for bitcoin mining

--- Report/bitcoin1.cpp
+++ Report/bitcoin1.cpp
@@ -1,10 +1,11 @@
 while (true) {
     BitcoinThread.signal_wait(0x1);
-    for(int i = 0; i < 5000; i++) {
+    //CONST_HASH_RATE = 5000
+    for(int i = 0; i < CONST_HASH_RATE; i++) {
         newKey_mutex.lock();
         *key = newKey;
         newKey_mutex.unlock();
-        sha256.computeHash(hash, sequence, 64);
+        hasher.computeHash(hash, sequence, 64);
         *nonce +=1;
 
         if(hash[0] == 0 && hash[1] == 0) {
